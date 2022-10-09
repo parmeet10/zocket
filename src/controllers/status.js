2948a09f2c29099feaf3e2ea9415147fcf6db4ad
parmeet <sparmeet162000@gmail.com>
2022-10-09 11:17:35 +0530

    minor fixes(for platforms, products

--- src/controllers/status.js
+++ src/controllers/status.js
@@ -4,7 +4,9 @@ const wrapperService = require("../services/wrapper");
 
 const getStatus = async (req, res, next) => {
   let statusParams = {};
-  req.query.statusId ? (statusParams.statusId = req.query.statusId) : null;
+  req.query.statusId
+    ? (statusParams.statusId = parseInt(req.query.statusId))
+    : null;
 
   let result = await statusService.getStatus(statusParams);
 
